e05343735d4de82906e0e46f575acc0c0a6af00c
manviyadav3322-rgb <manviyadav3322@gmail.com>
2026-03-29 15:48:44 +0530

    Update activity.ts

--- api/activity.ts
+++ api/activity.ts
@@ -1,1 +1,17 @@
+import { store } from "../data/store";
 
+export default function handler(req, res) {
+  const recentActivity = store.items
+    .slice()
+    .reverse()
+    .slice(0, 10)
+    .map((item) => ({
+      id: item.id,
+      action: `Added ${item.name}`,
+      date: item.date
+    }));
+
+  res.status(200).json({
+    recentActivity
+  });
+}
